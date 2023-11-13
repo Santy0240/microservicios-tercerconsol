31b04871188293a7fbb5518e6aa0fb08aa8dc5ca
santiago goez <128654665+Santy0240@users.noreply.github.com>
2023-11-12 21:34:27 -0500

    Acceso.py terminado

--- python/src/gateway/auth_svc/access.py
+++ python/src/gateway/auth_svc/access.py
@@ -6,5 +6,13 @@ def login(request):
         return None, ("missing credentials", 401)
     basicAuth = (auth.username, auth.password)
 
-    reponse = requests.post()
+    reponse = requests.post(
+            f"http://{os.environ.get('AUTH_SVC_ADDRESS')}/login", auth=basicAuth
+    )
+
+    if response.status_code == 200:
+        return response.txt, None
+    else:
+        return None, (response.txt, response.status_code)
+
     
